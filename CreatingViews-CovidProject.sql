5012c3973332c08fb0f2e8eb58051dc4c58a747f
Melina <130610741+its-melina@users.noreply.github.com>
2025-03-27 13:43:21 -0700

    updated views

--- CreatingViews-CovidProject.sql
+++ CreatingViews-CovidProject.sql
@@ -1,1 +1,9 @@
--- View for Highest Infection Counts in each location
+CREATE VIEW  HighestInfectionRate as
+
+	SELECT location, MAX(total_cases) as HighestInfectionCount, population, 
+	MAX(total_cases/population)*100 as HighestInfectionRate
+	FROM CovidDeaths
+	WHERE total_cases IS NOT NULL
+		AND population IS NOT NULL
+	GROUP BY population, location
+	ORDER BY 4 DESC;
