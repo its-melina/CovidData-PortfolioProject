466510ac77f9992bd2f3b091541586c823c24a95
Melina <130610741+its-melina@users.noreply.github.com>
2025-03-30 17:40:19 -0700

    updated queries

--- CreatingViews-CovidProject.sql
+++ CreatingViews-CovidProject.sql
@@ -1,3 +1,5 @@
+-- Highest-recorded infection rate and count for each location.
+
 CREATE VIEW  HighestInfectionRate as
 
 	SELECT location, MAX(total_cases) as HighestInfectionCount, population, 
@@ -7,3 +9,15 @@ CREATE VIEW  HighestInfectionRate as
 		AND population IS NOT NULL
 	GROUP BY population, location
 	ORDER BY 4 DESC;
+
+-- Percent of population vaccinated by location and date.
+
+CREATE VIEW PercentPopulationVaccinated as 
+	SELECT cd.continent, cd.location, cd.population, cv.new_vaccinations,
+	SUM(cv.new_vaccinations) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) 
+	as RollingPeopleVaccinated
+	FROM CovidDeaths as cd
+	JOIN CovidVaccinations as cv
+		ON cd.location = cv.location
+		AND cd.date = cv.date
+	WHERE cd.continent IS NOT null;
--- TempTable-CovidProject.sql
+++ TempTable-CovidProject.sql
@@ -1,9 +1,5 @@
-SELECT cd.continent, cd.location, cd.date, cd.population, cv.new_vaccinations
-	FROM CovidDeaths as cd
-	JOIN CovidVaccinations as cv
-		ON cd.location = cv.location
-		AND cd.date = cv.date
-	ORDER BY 1, 2, 3; 
+
+-- Using CTE
 
 -- Getting the rolling number of total vaccinations for each location
 -- Then, comparing the proportion of vaccinated people for each population
@@ -51,17 +47,5 @@ INSERT INTO VaccinesVsPop (continent, "location", population,
 
 SELECT *, (RollingPeopleVaccinated/population)*100 as PercentVaccinated
 FROM VaccinesVsPop;
-
--- Creating a View to save this query for later visualizations 
-
-CREATE VIEW PercentPopulationVaccinated as 
-	SELECT cd.continent, cd.location, cd.population, cv.new_vaccinations,
-	SUM(cv.new_vaccinations) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) 
-	as RollingPeopleVaccinated
-	FROM CovidDeaths as cd
-	JOIN CovidVaccinations as cv
-		ON cd.location = cv.location
-		AND cd.date = cv.date
-	WHERE cd.continent IS NOT null;
 
  
