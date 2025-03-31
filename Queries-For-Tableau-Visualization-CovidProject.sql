d3f4e8965a1b8640bd115bc82d94a327db6c791a
Melina <130610741+its-melina@users.noreply.github.com>
2025-03-30 17:34:38 -0700

    updated queries for Tableau

--- Queries-For-Tableau-Visualization-CovidProject.sql
+++ Queries-For-Tableau-Visualization-CovidProject.sql
@@ -8,15 +8,31 @@ Select SUM(new_cases) as total_cases, SUM(new_deaths) as total_deaths,
 
 -- 2. Total Deaths by Continent 
 
-Select continent, SUM(new_deaths) as total_cases
+Select continent, SUM(new_deaths) as total_deaths
 	From CovidDeaths 
 	where continent is not null
 	Group By continent 
 	Order By 2 Desc;
 
--- 3. Highest Death Percentage by Location
+-- 3. Highest Infection Rate by Location
 
-Select location, MAX(total_deaths), MAX(total_deaths/population)
+Select location, population, MAX(total_cases) as HighestInfectionCount, 
+	MAX((total_cases/population))*100 as PercentPopulationInfected
 	From CovidDeaths
-	Group By location
-	Order by 3, 1;
+	Group By location, population
+	Order By PercentPopulationInfected Desc;
+
+-- 4. Highest Infection Rate by Location and Date
+
+Select location, date, MAX(total_cases) as HighestInfectionCount,
+	MAX(total_cases/population)*100 as PercentPopulationInfected
+	From CovidDeaths
+	Group By location, population, date
+	Order by PercentPopulationInfected asc; 
+ 
+/*
+Select location, MAX(total_deaths), MAX(total_deaths/population)*100
+	From CovidDeaths
+	Group By location 
+	Order by 3 Desc;
+*/
